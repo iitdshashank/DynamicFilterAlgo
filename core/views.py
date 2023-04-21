1b10ab67518c9ef10bfba8d1008dd1cacd15d06c
iitdshashank <98643273+iitdshashank@users.noreply.github.com>
2023-04-22 01:33:53 +0530

    Add files via upload

---
+++ core/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from .models import Journal
+
+def BootstrapFilterView(request):
+    qs=Journal.objects.all()
+    title_contains_query=request.GET.get('title_contains')
+    title_exact_query=request.GET.get('title_exact')
+    title_or_author_query=request.GET.get('title_or_author')
+
+    if title_contains_query != '' and title_contains_query is not None:
+        qs=qs.filter(title__icontains=title_contains_query)
+
+    context={
+        'queryset':qs
+    }
+
+    return render(request, "bootstrap_form.html", context)
+
+
+
+
